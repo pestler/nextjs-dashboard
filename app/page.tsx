4fddf2d97a0a22c718a51948f0daf0720d383b00
Aleh Kuis <pestleralex@gmail.com>
2024-11-05 16:47:26 +0300

    feat: fetching Data vercel POSTGRES

--- app/page.tsx
+++ app/page.tsx
@@ -48,3 +48,24 @@ export default function Page() {
     </main>
   );
 }
+
+
+/* import { sql } from "@vercel/postgres";
+
+export default async function Cart({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+} */
